a7ede59b36dda82881a6ffb1d49fad3ac2f3e9bc
vonScholten <f.vonscholten.reg@gmail.com>
2017-11-09 10:34:29 +0100

    galgeleg v1: won & lost skærmbillede

---
+++ app/src/main/java/s145005/galgeleg_v1/GameWonActivity.java
@@ -0,0 +1,13 @@
+package s145005.galgeleg_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameWonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_won);
+    }
+}
