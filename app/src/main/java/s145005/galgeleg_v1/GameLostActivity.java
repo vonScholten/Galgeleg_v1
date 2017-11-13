b9c5757b4b4756195f529d33de39c2db0f40da27
vonScholten <f.vonscholten.reg@gmail.com>
2017-11-13 19:29:42 +0100

    return to main aktivity button added to game lost

--- app/src/main/java/s145005/galgeleg_v1/GameLostActivity.java
+++ app/src/main/java/s145005/galgeleg_v1/GameLostActivity.java
@@ -3,14 +3,20 @@ package s145005.galgeleg_v1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class GameLostActivity extends AppCompatActivity {
+public class GameLostActivity extends AppCompatActivity implements View.OnClickListener {
 
     String word;
 
     TextView loserText;
-    TextView loserWord;
+    TextView word_lost;
+
+    Button re_turn;
+
+    Intent returnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,23 @@ public class GameLostActivity extends AppCompatActivity {
             word = bundle.getString("lostWord");
 
         loserText = (TextView) findViewById(R.id.statusText);
-        loserWord = (TextView) findViewById(R.id.showAttemps);
+        word_lost = (TextView) findViewById(R.id.showAttemps);
 
-        loserWord.setText("Ordet var: " + word);
+        re_turn = (Button) findViewById(R.id.returnButton);
+        re_turn.setOnClickListener(this);
+
+        returnHome = new Intent(GameLostActivity.this, MainActivity.class);
+
+        word_lost.setText("Ordet var: " + word);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == re_turn) {
+            GameLostActivity.this.startActivity(returnHome);
+            System.out.println("return from GameLostActivity to MainActivity");
+        }
+
     }
 }
